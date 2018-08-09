ed450664964e9fe0268f190ee3a75e8059dfffcf
Tom Hombergs <tom.hombergs@gmail.com>
2018-08-10 00:09:11 +0200

    updated pact-feign example to Spring Boot 2 and JUnit 5

--- pact/pact-feign-consumer/src/main/java/io/reflectoring/ConsumerApplication.java
+++ pact/pact-feign-consumer/src/main/java/io/reflectoring/ConsumerApplication.java
@@ -2,10 +2,12 @@ package io.reflectoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClient(name = "userservice", configuration = RibbonConfiguration.class)
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
