8b5e2ce3a40ec09cd05805fadaa0628698c0fc4f
Petros Stergioulas <petros.stergioulas94@gmail.com>
2020-02-09 22:25:26 +0100

    Reflect-91 init

---
+++ spring-boot/spring-boot-openapi-impl/app/src/main/java/io/reflectoring/OpenAPIConsumerApp.java
@@ -0,0 +1,13 @@
+package io.reflectoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAPIConsumerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAPIConsumerApp.class, args);
+    }
+
+}
