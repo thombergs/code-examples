327e849575894b8986040f206cbe2f66b126cbd5
thombergs <tom.hombergs@gmail.com>
2020-02-20 21:38:00 +1100

    included flyway module in build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -28,6 +28,7 @@ build_gradle_module() {
 
 chmod +x gradlew
 
+build_gradle_module "spring-boot/data-migration/flyway"
 build_gradle_module "junit/assumptions"
 build_gradle_module "logging"
 build_gradle_module "pact/pact-feign-consumer"
