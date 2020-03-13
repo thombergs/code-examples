1f9fbc2543db9f5a679272cbc443d15425192589
thombergs <tom.hombergs@gmail.com>
2020-03-14 08:58:44 +1100

    changed class name

--- spring-boot/boundaries/src/main/java/io/reflectoring/boundaries/billing/internal/batchjob/internal/BillingBatchJobConfiguration.java
+++ spring-boot/boundaries/src/main/java/io/reflectoring/boundaries/billing/internal/batchjob/internal/BillingBatchJobConfiguration.java
@@ -7,6 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 @ComponentScan
-class BatchJobConfiguration {
+class BillingBatchJobConfiguration {
 
 }
