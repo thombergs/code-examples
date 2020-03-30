e023c7c42f8379c19807e05fb26e68122ad20e3e
thombergs <tom.hombergs@gmail.com>
2020-03-31 06:06:50 +1100

    polishing

--- spring-boot/spring-events/src/main/java/io/reflectoring/eventsdemo/Publisher.java
+++ spring-boot/spring-events/src/main/java/io/reflectoring/eventsdemo/Publisher.java
@@ -1,27 +1,28 @@
 package io.reflectoring.eventsdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 class Publisher {
 
-	@Autowired
-	private ApplicationEventPublisher publisher;
+  private final ApplicationEventPublisher publisher;
 
-	void publishEvent() {
-		// Async Event		
-		publisher.publishEvent("I'm Async");
-		
-		
-		// @EventListener Annotated and ApplicationListener			
-	    publisher.publishEvent(new UserCreatedEvent(this, "Lucario"));
-	    publisher.publishEvent(new UserRemovedEvent("Lucario"));
-	    
-	    // Conditional Listener	    
-	    publisher.publishEvent(new UserCreatedEvent(this, "reflectoring"));
-	    publisher.publishEvent(new UserRemovedEvent("reflectoring"));
-	    
-	}
+  Publisher(ApplicationEventPublisher publisher) {
+    this.publisher = publisher;
+  }
+
+  void publishEvent() {
+    // Async Event
+    publisher.publishEvent("I'm Async");
+
+    // @EventListener Annotated and ApplicationListener
+    publisher.publishEvent(new UserCreatedEvent(this, "Lucario"));
+    publisher.publishEvent(new UserRemovedEvent("Lucario"));
+
+    // Conditional Listener
+    publisher.publishEvent(new UserCreatedEvent(this, "reflectoring"));
+    publisher.publishEvent(new UserRemovedEvent("reflectoring"));
+
+  }
 }
