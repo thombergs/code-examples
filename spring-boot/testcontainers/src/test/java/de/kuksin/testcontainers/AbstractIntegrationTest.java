1a4c9421dcb3d6c180ce895c677ababdf5927796
akuksin <andi0000>
2020-04-22 23:56:35 +0200

    clean up test

--- spring-boot/testcontainers/src/test/java/de/kuksin/testcontainers/AbstractIntegrationTest.java
+++ spring-boot/testcontainers/src/test/java/de/kuksin/testcontainers/AbstractIntegrationTest.java
@@ -1,21 +1,16 @@
 package de.kuksin.testcontainers;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.lifecycle.Startables;
 
 import java.util.Map;
 import java.util.stream.Stream;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
 
