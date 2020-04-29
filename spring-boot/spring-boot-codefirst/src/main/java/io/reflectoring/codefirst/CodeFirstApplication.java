bb7ac442b17465863e3e0281b49a8c34bcb8fd40
Petros Stergioulas <petros.stergioulas94@gmail.com>
2020-04-29 20:07:36 +0200

    Init

---
+++ spring-boot/spring-boot-codefirst/src/main/java/io/reflectoring/codefirst/CodeFirstApplication.java
@@ -0,0 +1,13 @@
+package io.reflectoring.codefirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeFirstApplication.class, args);
+    }
+
+}
---
+++ spring-boot/spring-boot-codefirst/src/test/java/io/reflectoring/codefirst/CodeFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.codefirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
