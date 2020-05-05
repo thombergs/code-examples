f183c92661a0b3f8708cfcd5779637844206ec57
akuksin <andi0000>
2020-05-05 17:14:24 +0200

    refactoring

--- spring-boot/testcontainers/src/test/java/de/kuksin/testcontainers/AbstractIntegrationTest.java
+++ spring-boot/testcontainers/src/test/java/de/kuksin/testcontainers/AbstractIntegrationTest.java
@@ -1,5 +1,6 @@
 package de.kuksin.testcontainers;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -19,10 +20,13 @@ public class AbstractIntegrationTest {
 
         static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>();
 
-        public static Map<String, String> getProperties() {
+        private static void startContainers() {
             Startables.deepStart(Stream.of(postgres)).join();
             // we can add further containers here like rabbitmq or other database
+        }
 
+        @NotNull
+        private static Map<String, String> createConnectionConfiguration() {
             return Map.of(
                     "spring.datasource.url", postgres.getJdbcUrl(),
                     "spring.datasource.username", postgres.getUsername(),
@@ -33,10 +37,11 @@ public class AbstractIntegrationTest {
 
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContainers();
             ConfigurableEnvironment environment = applicationContext.getEnvironment();
             MapPropertySource testcontainers = new MapPropertySource(
                     "testcontainers",
-                    (Map) getProperties()
+                    (Map) createConnectionConfiguration()
             );
             environment.getPropertySources().addFirst(testcontainers);
         }
