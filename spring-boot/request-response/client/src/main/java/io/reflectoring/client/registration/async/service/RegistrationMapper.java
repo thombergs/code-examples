228767ec9b1bc41527647c6a74a355ae2cf887d2
akuksin <andi0000>
2020-07-29 19:15:28 +0200

    Make method package private in the mapper.

--- spring-boot/request-response/client/src/main/java/io/reflectoring/client/registration/async/service/RegistrationMapper.java
+++ spring-boot/request-response/client/src/main/java/io/reflectoring/client/registration/async/service/RegistrationMapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class RegistrationMapper {
 
-    public Registration toRegistration(RegistrationDto registrationDto){
+    Registration toRegistration(RegistrationDto registrationDto){
         return Registration.builder()
                 .id(registrationDto.getId())
                 .date(registrationDto.getDate())
