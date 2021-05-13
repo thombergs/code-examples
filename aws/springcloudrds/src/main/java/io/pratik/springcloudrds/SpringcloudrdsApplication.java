7fd1bdc29f1f122c2e26802026c8968a19c38c44
Pratik Das <pratikd2000@gmail.com>
2021-05-13 15:56:32 +0400

    added rds code

---
+++ aws/springcloudrds/src/main/java/io/pratik/springcloudrds/SpringcloudrdsApplication.java
@@ -0,0 +1,13 @@
+package io.pratik.springcloudrds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudrdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudrdsApplication.class, args);
+	}
+
+}
