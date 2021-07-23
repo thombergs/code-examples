2e74db86844f68f7d14b0a400c1478d648b15d82
Tom Hombergs <tom.hombergs@gmail.com>
2021-07-24 06:44:33 +1000

    wip

--- spring-boot/zero-downtime/src/main/java/io/reflectoring/zerodowntime/CustomerController.java
+++ spring-boot/zero-downtime/src/main/java/io/reflectoring/zerodowntime/CustomerController.java
@@ -6,26 +6,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 
-    private final CustomerRepository customerRepository;
+    private final CustomerRepository oldCustomerRepository;
+    private final NewCustomerRepository newCustomerRepository;
+    private final FeatureFlagService featureFlagService;
 
-    public CustomerController(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
+    public CustomerController(CustomerRepository oldCustomerRepository, NewCustomerRepository newCustomerRepository, FeatureFlagService featureFlagService) {
+        this.oldCustomerRepository = oldCustomerRepository;
+        this.newCustomerRepository = newCustomerRepository;
+        this.featureFlagService = featureFlagService;
     }
 
     @GetMapping("/customers/create")
     String createUser() {
-        Customer customer = new Customer("Bob", "Builder", "21 Build Street");
-        customerRepository.save(customer);
+        if (featureFlagService.writeToNewCustomerSchema()) {
+            NewCustomer customer = new NewCustomer("Bob", "Builder", "Build Street", "21");
+            newCustomerRepository.save(customer);
+        } else {
+            OldCustomer customer = new OldCustomer("Bob", "Builder", "21 Build Street");
+            oldCustomerRepository.save(customer);
+        }
         return "customer created";
     }
 
     @GetMapping("/customers/list")
     String showUser() {
-        Iterable<Customer> customers = customerRepository.findAll();
         StringBuffer buffer = new StringBuffer();
-        for (Customer customer : customers) {
-            buffer.append("\n");
-            buffer.append(customer.toString());
+        if (featureFlagService.readFromNewCustomerSchema()) {
+            Iterable<NewCustomer> customers = newCustomerRepository.findAll();
+            for (NewCustomer customer : customers) {
+                buffer.append("\n");
+                buffer.append(customer.toString());
+            }
+        } else {
+            Iterable<OldCustomer> customers = oldCustomerRepository.findAll();
+            for (OldCustomer customer : customers) {
+                buffer.append("\n");
+                buffer.append(customer.toString());
+            }
         }
         return buffer.toString();
     }
