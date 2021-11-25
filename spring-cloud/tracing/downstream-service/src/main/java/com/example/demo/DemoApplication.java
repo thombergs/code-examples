6aa921d29b964431259e03cce76d920ad6f2ea3b
Tom Hombergs <tom.hombergs@gmail.com>
2021-11-25 20:43:09 +1100

    migrate to Maven

--- spring-cloud/tracing/downstream-service/src/main/java/com/example/demo/DemoApplication.java
+++ spring-cloud/tracing/downstream-service/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoApplication {
 
   public static void main(String[] args) {
