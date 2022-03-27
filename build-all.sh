1f37a81b90003c35beed6216432f689936917cb1
Saikat Sengupta <41847480+s4ik4t@users.noreply.github.com>
2022-03-28 03:03:11 +0530

    Add code example for Spring Boot Application Info (#162)
    * Add code example for Spring Boot Application Info
    
    * Add relevant files

--- build-all.sh
+++ build-all.sh
@@ -100,6 +100,7 @@ fi
 
 if [[ "$MODULE" == "module6" ]]
 then
+  build_maven_module "spring-boot/spring-boot-app-info"
   build_maven_module "spring-boot/spring-boot-null-safe-annotations"
   build maven_module "aws/cdkv2"
   build_maven_module "immutables"
