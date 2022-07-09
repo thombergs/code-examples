55d33b24cf5e2cbc10052d00772969fad3c9671a
Jarjanazy <celil.cercenazi@gmail.com>
2022-07-09 12:17:51 +0200

    add a few code examples

--- core-java/jackson/jackson/src/test/java/com/reflectoring/JacksonTest.java
+++ core-java/jackson/jackson/src/test/java/com/reflectoring/JacksonTest.java
@@ -1,12 +1,15 @@
 package com.reflectoring;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import com.reflectoring.pojo.Employee;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -57,5 +60,28 @@ public class JacksonTest {
         Employee employee = objectMapper.readValue(employeeJson.getBytes(), Employee.class);
 
         assertThat(employee.getFirstName()).isEqualTo("Jalil");
+    }
+
+    @Test
+    void fileToListOfPojos() throws IOException {
+        File file = new File("src/test/resources/employeeList.json");
+        List<Employee> employeeList = objectMapper.readValue(file, new TypeReference<>(){});
+
+        assertThat(employeeList).hasSize(2);
+        assertThat(employeeList.get(0).getAge()).isEqualTo(33);
+        assertThat(employeeList.get(0).getLastName()).isEqualTo("Simpson");
+        assertThat(employeeList.get(0).getFirstName()).isEqualTo("Marge");
+    }
+
+    @Test
+    void fileToMap() throws IOException {
+        File file = new File("src/test/resources/employee.json");
+        Map<String, Object> employee = objectMapper.readValue(file, new TypeReference<>(){});
+
+        assertThat(employee.keySet()).containsExactly("firstName", "lastName", "age");
+
+        assertThat(employee.get("firstName")).isEqualTo("Homer");
+        assertThat(employee.get("lastName")).isEqualTo("Simpson");
+        assertThat(employee.get("age")).isEqualTo(44);
     }
 }
