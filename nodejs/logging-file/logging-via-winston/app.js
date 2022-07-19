9618b56f6683a5ddd14bcb23f87cbef4388cb17b
ajibade3210 <ajibade69th@gmail.com>
2022-07-19 18:18:32 +0100

    Adding NodeJs Logging CodeExamples

--- nodejs/logging-file/logging-via-winston/app.js
+++ nodejs/logging-file/logging-via-winston/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const logger = require("./logger-log-rotation");
+const logger = require("./logger");
 const app = express();
 
 app.get("/", (req, res, next) => {
