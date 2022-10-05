a419a08b8d6a8da46ed5506a50c4107c1f0c6083
Jarjanazy <celil.cercenazi@gmail.com>
2022-10-05 23:03:03 +0200

    add initial project for AOP post

---
+++ spring-boot/spring-aop/src/main/java/io/reflectoring/springboot/aop/AopApplication.java
@@ -0,0 +1,13 @@
+package io.reflectoring.springboot.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopApplication.class, args);
+	}
+
+}
---
+++ spring-boot/spring-aop/src/test/java/io/reflectoring/springboot/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.springboot.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
