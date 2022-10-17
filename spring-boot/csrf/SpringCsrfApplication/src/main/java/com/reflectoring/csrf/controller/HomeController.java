143d514c982fe4083c812ae761da442a95706f4d
ranjani2412 <53815317+ranjani2412@users.noreply.github.com>
2022-10-17 16:27:57 +1100

    Test updates

--- spring-boot/csrf/SpringCsrfApplication/src/main/java/com/reflectoring/csrf/controller/HomeController.java
+++ spring-boot/csrf/SpringCsrfApplication/src/main/java/com/reflectoring/csrf/controller/HomeController.java
@@ -2,21 +2,16 @@ package com.reflectoring.csrf.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String homePage(HttpServletResponse response) {
         ResponseCookie responseCookie = ResponseCookie.from("testCookie", "cookieVal")
                 .sameSite("Lax")
--- spring-boot/csrf/SpringCsrfApplication/src/test/java/config/SecurityConfiguration.java
+++ spring-boot/csrf/SpringCsrfApplication/src/test/java/config/SecurityConfiguration.java
@@ -1,13 +1,11 @@
-package com.reflectoring.csrf.config;
+package config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
@@ -35,13 +33,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll().and().httpBasic().and().formLogin()
                 .successHandler(loginSuccessHandler())
                 //.and().csrf().disable();
-                .and()
-                .csrf()
+                //.and()
+                //.csrf()
                 //.csrf().csrfTokenRepository(csrfTokenRepository())
-                //.ignoringAntMatchers(patterns);
-                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/login"))
-                .and().csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/home"))
-                .and().csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/registerEmail"));
+                //.ignoringAntMatchers(patterns)
+                //.requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/login"))
+                //.and().csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/home"))
+                .and().csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("/registerEmail"));
                 //.and()
                 //.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
                 //.httpBasic().and().formLogin().permitAll();
