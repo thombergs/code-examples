7076da2ceea85f1f1cd1f34e484476d61d5a24fd
ranjani2412 <53815317+ranjani2412@users.noreply.github.com>
2022-10-18 07:26:18 +1100

    Updates to code

--- spring-boot/csrf/SpringCsrfApplication/src/main/java/com/reflectoring/csrf/controller/HomeController.java
+++ spring-boot/csrf/SpringCsrfApplication/src/main/java/com/reflectoring/csrf/controller/HomeController.java
@@ -2,13 +2,14 @@ package com.reflectoring.csrf.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/home")
