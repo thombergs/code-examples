defb8128c5b4c24ed8cdb5233f638adf8a9985ad
Muhammad Irtaza <66386453+Irtaza2009@users.noreply.github.com>
2023-05-21 00:28:09 +0500

    Create bitwise-operators.ts

---
+++ nodejs/operators-in-typescript/bitwise-operators.ts
@@ -0,0 +1,9 @@
+let a = 5; // 0101 in binary
+let b = 3; // 0011 in binary
+
+console.log(a & b); // 0001 (AND)
+console.log(a | b); // 0111 (OR)
+console.log(a ^ b); // 0110 (XOR)
+console.log(a << 1); // 1010 (Left Shift)
+console.log(a >> 1); // 0010 (Right Shift)
+console.log(~a); // 1010 (Complement)
