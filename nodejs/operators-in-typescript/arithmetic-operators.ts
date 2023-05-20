8ed932f4e40ffeca9a84abd6045cfa21dd89d0a2
Muhammad Irtaza <66386453+Irtaza2009@users.noreply.github.com>
2023-05-21 00:26:31 +0500

    Update arithmetic-operators.ts

--- nodejs/operators-in-typescript/arithmetic-operators.ts
+++ nodejs/operators-in-typescript/arithmetic-operators.ts
@@ -1,5 +1,5 @@
-let x: number = 5;
-let y: number = 10;
+let x = 5;
+let y = 10;
 
 console.log(x + y); // Output: 15
 console.log(x - y); // Output: -5
@@ -7,10 +7,10 @@ console.log(x * y); // Output: 50
 console.log(x / y); // Output: 0.5
 console.log(y % x); // Output: 0
 
-let z: number = 3;
+let z = 3;
 z++; 
 console.log(z);   // Output: 4
 
-let a: number = 10;
+let a = 10;
 a--;
-console.log(a);   // Output: 11
+console.log(a);   // Output: 9
