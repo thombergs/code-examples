b73538654d1034a7496e1d673f1a93c78463db1a
sagaofsilence <sagaofsilence@gmail.com>
2023-12-08 15:17:34 +0530

    Updated the record and its test

--- core-java/records/src/main/java/io/reflectoring/records/Person.java
+++ core-java/records/src/main/java/io/reflectoring/records/Person.java
@@ -9,19 +9,51 @@ public record Person(
     @Min(value = 0, message = "Age cannot be lesser that 0")
         @Max(value = 130, message = "Age cannot be greater that 130")
         int age) {
+
+  private static final String DEFAULT_NAME = "Foo";
+  private static final int DEFAULT_AGE = 50;
+  private static final int MIN_AGE = 0;
+  private static final int ADULT_MIN_AGE = 18;
+  private static final int MAX_AGE = 130;
+
+  public Person {
+    if (name == null || name.isBlank()) {
+      throw new IllegalArgumentException("Got invalid name.");
+    }
+    if (age < MIN_AGE || age > MAX_AGE) {
+      throw new IllegalArgumentException("Got invalid age");
+    }
+  }
+
   public Person() {
-    this("Foo", 50);
+    this(DEFAULT_NAME, DEFAULT_AGE);
   }
 
   public Person(String name) {
-    this(name, 50);
+    this(name, DEFAULT_AGE);
   }
 
   public boolean isNameValid() {
     return !(name == null || name.isBlank());
   }
 
-  public Person withName(String name) {
+  public boolean isAgeValid() {
+    return age > MIN_AGE && age < MAX_AGE;
+  }
+
+  public boolean isAdult() {
+    return age >= ADULT_MIN_AGE;
+  }
+
+  public Person withInput(String name, int age) {
     return new Person(name, age);
+  }
+
+  public Person withName(String name) {
+    return new Person(name, DEFAULT_AGE);
+  }
+
+  public Person withAge(int age) {
+    return new Person(DEFAULT_NAME, age);
   }
 }
--- core-java/records/src/test/java/io/reflectoring/records/PersonTest.java
+++ core-java/records/src/test/java/io/reflectoring/records/PersonTest.java
@@ -1,38 +1,28 @@
 package io.reflectoring.records;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.SoftAssertions;
-import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class PersonTest {
-  private static final Validator VALIDATOR =
-      Validation.byDefaultProvider()
-          .configure()
-          .messageInterpolator(new ParameterMessageInterpolator())
-          .buildValidatorFactory()
-          .getValidator();
 
   @Test
   void allArgConstructor() {
-    Person bob = new Person("Bob", 40);
-    assertThat(bob)
+    final Person bar = new Person("Bar", 40);
+    assertThat(bar)
         .as("Person cannot be null.")
         .isNotNull()
-        .hasToString("Person[name=Bob, age=40]");
-    log.info("Bob {}", bob);
+        .hasToString("Person[name=Bar, age=40]");
+    log.info("Bar {}", bar);
   }
 
   @Test
   void compactConstructor() {
-    Person person = new Person();
+    final Person person = new Person();
     assertThat(person)
         .as("Person cannot be null.")
         .isNotNull()
@@ -42,7 +32,7 @@ class PersonTest {
 
   @Test
   void nameArgConstructor() {
-    Person person = new Person("Tom");
+    final Person person = new Person("Tom");
     assertThat(person)
         .as("Person cannot be null.")
         .isNotNull()
@@ -52,22 +42,32 @@ class PersonTest {
 
   @Test
   void name() {
-    Person person = new Person();
+    final Person person = new Person();
     assertThat(person).as("Person cannot be null.").isNotNull().extracting("name").isEqualTo("Foo");
     log.info("Person {}", person);
   }
 
   @Test
   void age() {
-    Person person = new Person();
+    final Person person = new Person();
     assertThat(person).as("Person cannot be null.").isNotNull().extracting("age").isEqualTo(50);
     log.info("Person {}", person);
   }
 
   @Test
+  void adult() {
+    final Person adultPerson = new Person("Tom", 20);
+    assertThat(adultPerson.isAdult()).as("Person should be adult.").isTrue();
+    log.info("Adult Person {}", adultPerson);
+
+    final Person minorPerson = new Person("Jerry", 10);
+    assertThat(minorPerson.isAdult()).as("Person should not be adult.").isFalse();
+    log.info("Minor Person {}", minorPerson);
+  }
+  @Test
   void helperMethods() {
-    Person fooPerson = new Person("Foo", 40);
-    Person barPerson = new Person("Bar", 40);
+    final Person fooPerson = new Person("Foo", 40);
+    final Person barPerson = new Person("Bar", 40);
     SoftAssertions personBundle = new SoftAssertions();
     personBundle.assertThat(fooPerson).isNotEqualTo(barPerson);
     personBundle.assertThat(fooPerson.toString()).isNotEmpty();
@@ -82,7 +82,7 @@ class PersonTest {
 
   @Test
   void delegateMethod() {
-    Person person = new Person();
+    final Person person = new Person();
     final Person newPerson = person.withName("Tom");
     assertThat(newPerson)
         .as("Person cannot be null.")
@@ -94,16 +94,14 @@ class PersonTest {
 
   @Test
   void invalidAge() {
-    final Person invalidPerson = new Person("Bob", 132);
-
-    final Set<ConstraintViolation<Person>> constraintViolations = VALIDATOR.validate(invalidPerson);
-
-    log.info("Got {} constraint violations: {}", constraintViolations.size(), constraintViolations);
-    assertThat(constraintViolations)
-        .filteredOn(
-            personConstraintViolation ->
-                personConstraintViolation.getMessage().equals("Age cannot be greater that 130"))
-        .hasSize(1);
+    final Throwable thrown =
+        catchThrowable(
+            () -> {
+              new Person("Bob", 132);
+            });
+    assertThat(thrown)
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("Got invalid age");
   }
 
   @Test
