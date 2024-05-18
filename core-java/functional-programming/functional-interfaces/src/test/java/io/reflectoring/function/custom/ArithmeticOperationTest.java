ced017fd03348189990c14311a27b8f13d6830b5
sagaofsilence <sagaofsilence@gmail.com>
2024-05-18 15:47:35 +0530

    Clean up

--- core-java/functional-programming/functional-interfaces/src/test/java/io/reflectoring/function/custom/ArithmeticOperationTest.java
+++ core-java/functional-programming/functional-interfaces/src/test/java/io/reflectoring/function/custom/ArithmeticOperationTest.java
@@ -14,16 +14,10 @@ class ArithmeticOperationTest {
     ArithmeticOperation multiply = (a, b) -> a * b;
     ArithmeticOperation divide = (a, b) -> a / b;
 
-    // Use the operations
-    int addition = add.operate(10, 5); // Returns 15
-    int subtraction = subtract.operate(10, 5); // Returns 5
-    int multiplication = multiply.operate(10, 5); // Returns 50
-    int division = divide.operate(10, 5); // Returns 2
-
     // Verify results
-    assertEquals(15, addition, "Result of addition is not correct.");
-    assertEquals(5, subtraction, "Result of subtraction is not correct.");
-    assertEquals(50, multiplication, "Result of multiplication is not correct.");
-    assertEquals(2, division, "Result of division is not correct.");
+    assertEquals(15, add.operate(10, 5));
+    assertEquals(5, subtract.operate(10, 5));
+    assertEquals(50, multiply.operate(10, 5));
+    assertEquals(2, divide.operate(10, 5));
   }
 }
