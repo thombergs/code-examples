93a6a51d4247ff717081e79ef32b359dd60c0c68
sagaofsilence <sagaofsilence@gmail.com>
2024-06-08 21:41:13 +0530

    Added test for assertTimeout

--- junit/junit5/functional-interfaces/src/test/java/io/reflectoring/functional/ExecutableTest.java
+++ junit/junit5/functional-interfaces/src/test/java/io/reflectoring/functional/ExecutableTest.java
@@ -2,12 +2,15 @@ package io.reflectoring.functional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.opentest4j.AssertionFailedError;
 
 public class ExecutableTest {
 
@@ -59,5 +62,28 @@ public class ExecutableTest {
               }
             });
     assertEquals("Got invalid value", exception.getMessage());
+  }
+
+  @Test
+  void testAssertTimeoutWithExecutable() {
+    List<Long> numbers = Arrays.asList(100L, 200L, 50L, 300L, 5L, 0L, 12L, 80L);
+    final int delay = 2;
+    assertThrows(
+        AssertionFailedError.class,
+        () ->
+            assertTimeout(
+                Duration.ofSeconds(1),
+                () -> {
+                  numbers.sort(Long::compareTo);
+                  TimeUnit.SECONDS.sleep(delay);
+                }));
+    assertDoesNotThrow(
+        () ->
+            assertTimeout(
+                Duration.ofSeconds(5),
+                () -> {
+                  numbers.sort(Long::compareTo);
+                  TimeUnit.SECONDS.sleep(delay);
+                }));
   }
 }
