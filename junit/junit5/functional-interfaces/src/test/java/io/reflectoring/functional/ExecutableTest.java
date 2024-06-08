f013132ea8085bc798f8b57a71a3711a4ac0f54d
sagaofsilence <sagaofsilence@gmail.com>
2024-06-08 19:36:05 +0530

    Added assertAll and assertDoesNotThrow test

---
+++ junit/junit5/functional-interfaces/src/test/java/io/reflectoring/functional/ExecutableTest.java
@@ -0,0 +1,43 @@
+package io.reflectoring.functional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class ExecutableTest {
+
+  @ParameterizedTest
+  @CsvSource({"1,1,2,Hello,H,bye,2,byebye", "4,5,9,Good,Go,Go,-10,", "10,21,31,Team,Tea,Stop,-2,"})
+  void testAssertAllExecutable(
+      int num1,
+      int num2,
+      int sum,
+      String input,
+      String prefix,
+      String arg,
+      int count,
+      String result) {
+    assertAll(
+        () -> assertEquals(sum, num1 + num2),
+        () -> assertTrue(input.startsWith(prefix)),
+        () -> {
+          if (count < 0) {
+            assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                  new ArrayList<>(count);
+                });
+          } else {
+            assertEquals(result, arg.repeat(count));
+          }
+        });
+  }
+
+  @ParameterizedTest
+  @CsvSource({"one,0,o", "one,1,n"})
+  void testAssertDoesNotThrow(String input, int index, char result) {
+    assertDoesNotThrow(() -> assertEquals(input.charAt(index), result));
+  }
+}
