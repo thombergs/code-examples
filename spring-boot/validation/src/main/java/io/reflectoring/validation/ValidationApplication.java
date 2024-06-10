06688c31f3f940c851346dcb3941def034461818
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-06-10 12:12:22 +0530

    externalizing error messages

--- spring-boot/validation/src/main/java/io/reflectoring/validation/ValidationApplication.java
+++ spring-boot/validation/src/main/java/io/reflectoring/validation/ValidationApplication.java
@@ -2,11 +2,22 @@ package io.reflectoring.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ValidationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ValidationApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ValidationApplication.class, args);
+  }
+
+  @Bean
+  public MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:validation-errors");
+    return messageSource;
+  }
+
 }
