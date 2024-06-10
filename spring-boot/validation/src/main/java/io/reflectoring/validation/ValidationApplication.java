717ac53511d8675d175df32c803cf34ebf15b4f9
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-06-10 13:06:45 +0530

    adding configuration properties example

--- spring-boot/validation/src/main/java/io/reflectoring/validation/ValidationApplication.java
+++ spring-boot/validation/src/main/java/io/reflectoring/validation/ValidationApplication.java
@@ -2,11 +2,13 @@ package io.reflectoring.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RequiredConfigurationProperties.class)
 public class ValidationApplication {
 
   public static void main(String[] args) {
