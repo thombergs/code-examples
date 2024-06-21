efc57430926912ef221db4586e27a00e2ca027c2
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-06-21 11:26:42 +0530

    section codebase: Loading Error Messages from a Properties File

--- spring-boot/validation/src/main/java/io/reflectoring/validation/Input.java
+++ spring-boot/validation/src/main/java/io/reflectoring/validation/Input.java
@@ -14,13 +14,13 @@ public class Input {
   @GeneratedValue
   private Long id;
 
-  @Min(1)
-  @Max(10)
+  @Min(value = 1, message = "{number.invalid}")
+  @Max(value = 10, message = "{number.invalid}")
   private int numberBetweenOneAndTen;
 
   // Note that this is actually not a valid IP address pattern, since
   // it allows values greater than 255 per octet.
-  @Pattern(regexp = "^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$")
+  @Pattern(regexp = "^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$", message = "{ip-address.invalid}")
   private String ipAddress;
 
   public int getNumberBetweenOneAndTen() {
