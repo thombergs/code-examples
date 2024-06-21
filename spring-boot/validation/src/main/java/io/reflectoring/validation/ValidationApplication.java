4e0c8ba5a953a6349d148e0521e123351fd5e251
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-06-21 11:11:06 +0530

    section codebase: Validating Properties at Startup

--- spring-boot/validation/src/main/java/io/reflectoring/validation/ValidationApplication.java
+++ spring-boot/validation/src/main/java/io/reflectoring/validation/ValidationApplication.java
@@ -2,8 +2,10 @@ package io.reflectoring.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RequiredConfigurationProperties.class)
 public class ValidationApplication {
 
 	public static void main(String[] args) {
